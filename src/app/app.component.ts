95c239cb620806e05c78e96523620e53aa83121f
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2023-01-19 14:12:30 -0600

    (feat) redirect page after form is complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'products_page';
+  constructor(private router:Router){}
+  add_products(){
+    this.router.navigate(['/products']);
+  }
 }
